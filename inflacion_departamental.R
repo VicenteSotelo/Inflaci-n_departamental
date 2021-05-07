b1201f27e0dff60e5e4da958c42586d229785913
Vicente Sotelo <lvs6@georgetown.edu>
2021-05-07 20:45:17 +0200

    Add files via upload

---
+++ inflacion_departamental.R
@@ -0,0 +1,53 @@
+
+# Inflación por departamento 
+# Autor: Vicente Sotelo
+# Fecha:  07/05/21
+
+# Seting up
+
+setwd("~/Downloads")
+
+library(ggplot2)
+
+library(dplyr)
+
+library(readxl)
+
+library(tidyverse)
+
+library(grid)
+
+
+#  Seting + transforming df
+
+t_df <-read_excel('./ipc_by_department.xls') %>% as_tibble(df) %>% drop_na()
+
+t_df$infl_marzo_21 <- ((t_df$`Marzo-21`/ t_df$`Marzo-20`)-1)*100
+
+d_lima=ifelse(t_df$DEPARTAMENTO=="LIMA",1,0)
+
+t_df <- cbind(t_df,d_lima)
+
+
+#  Mapping
+
+ggplot(data=t_df,mapping=aes(x=reorder(DEPARTAMENTO,infl_marzo_21),y=infl_marzo_21,fill=d_lima)) +
+
+geom_bar(stat="identity")+
+
+theme_bw(base_size=10) +
+
+coord_flip()+
+
+geom_hline(yintercept=3, linetype="dashed", 
+                color = "blue", size=1) +
+
+geom_hline(yintercept=1, linetype="dashed", 
+                color = "blue", size=1) +
+
+ggtitle("Inflación anual:Marzo-2021")+
+ 
+labs(y="Var.% anual del IPC ",x="departamentos", caption="Fuente XX")
+
+
+
